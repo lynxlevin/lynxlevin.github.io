cf3fe30c81bbd12f0e340cf761942c9657244244
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-12-18 06:40:49 +0900

    try

---
+++ xmas.js
@@ -0,0 +1,46 @@
+
+    window.addEventListener('DOMContentLoaded', ()=> {
+      // コンテナを指定
+      const container = document.querySelector('.snow-container');
+      const letItSnow = document.querySelector('.let-it-snow');
+      let count = 0;
+
+      letItSnow.addEventListener("click", () => {
+        if (count < 3) {
+          count++;
+          // 雪を生成する間隔をミリ秒で指定
+          setInterval(() => {createSnow(container)}, 100);
+        }
+      });
+
+      const jingleBells = document.querySelector('.jingle-bells');
+      const bellSound = new Audio('bells.mp3');
+
+      jingleBells.addEventListener("click", () => {
+        if (!jingleBells.classList.contains('playing')) {
+          jingleBells.classList.add('playing');
+          bellSound.currentTime = 1;
+          bellSound.play();
+        } else {
+          jingleBells.classList.remove('playing');
+          bellSound.pause();
+        }
+      });
+
+      const createSnow = (container) => {
+      const snowEl = document.createElement('span');
+      snowEl.className = 'snow';
+      const minSize = 5;
+      const maxSize = 10;
+      const size = Math.random() * (maxSize - minSize) + minSize;
+      snowEl.style.width = `${size}px`;
+      snowEl.style.height = `${size}px`;
+      snowEl.style.left = Math.random() * 100 + '%';
+      container.appendChild(snowEl);
+
+      // 一定時間が経てば雪を消す
+      setTimeout(() => {
+        snowEl.remove();
+      }, 10000);
+    }
+    });
