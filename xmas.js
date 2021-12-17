3e6371c31f403d473df61a2dda2bfba7a5315eef
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-12-18 07:17:35 +0900

    kill video

--- xmas.js
+++ xmas.js
@@ -21,22 +21,22 @@ class Aframe {
         });
       }
     });
-    AFRAME.registerComponent('videohandler', {
-        init: function () {
-            var marker = this.el;
-            this.vid = document.querySelector("#vid");
+    // AFRAME.registerComponent('videohandler', {
+    //     init: function () {
+    //         var marker = this.el;
+    //         this.vid = document.querySelector("#vid");
 
-            marker.addEventListener('markerFound', function () {
-                this.toggle = true;
-                this.vid.play();
-            }.bind(this));
+    //         marker.addEventListener('markerFound', function () {
+    //             this.toggle = true;
+    //             this.vid.play();
+    //         }.bind(this));
 
-            marker.addEventListener('markerLost', function () {
-                this.toggle = false;
-                this.vid.pause();
-            }.bind(this));
-        },
-    });
+    //         marker.addEventListener('markerLost', function () {
+    //             this.toggle = false;
+    //             this.vid.pause();
+    //         }.bind(this));
+    //     },
+    // });
   }
 }
 
