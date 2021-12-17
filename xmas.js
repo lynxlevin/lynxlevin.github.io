d81db2d41cb63eb39c237f523b0cd1f0a7df29a5
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2021-12-18 08:34:32 +0900

    scripts work

--- xmas.js
+++ xmas.js
@@ -1,69 +1,9 @@
-// class Aframe {
-//   constructor() {
-//       this.register();
-//   }
-
-//   register() {
-//     //マーカーの前に読み込まないと動かない
-//     AFRAME.registerComponent('registerevents', {
-//       init: function () {
-//         const marker = this.el;
-//         const container = document.querySelector('.snow-container');
-
-//         // マーカーを検出したイベントの登録
-//         marker.addEventListener('markerFound', async function () {
-//           console.log(marker.dataset.contentid);
-//           // setInterval(() => {createSnow(container)}, 100);
-//         });
-
-//         // マーカーを見失ったイベントの登録
-//         marker.addEventListener('markerLost', async function () {
-//         });
-//       }
-//     });
-//     // AFRAME.registerComponent('videohandler', {
-//     //     init: function () {
-//     //         var marker = this.el;
-//     //         this.vid = document.querySelector("#vid");
-
-//     //         marker.addEventListener('markerFound', function () {
-//     //             this.toggle = true;
-//     //             this.vid.play();
-//     //         }.bind(this));
-
-//     //         marker.addEventListener('markerLost', function () {
-//     //             this.toggle = false;
-//     //             this.vid.pause();
-//     //         }.bind(this));
-//     //     },
-//     // });
-//   }
-// }
-
-
-
-
-
-
 window.addEventListener('DOMContentLoaded', ()=> {
+  const bellSound = document.querySelector("#bgm1");
   const button = document.querySelector('#button');
   button.addEventListener("click", () => {
     button.classList.add('hidden');
+    bellSound.play();
+    bellSound.pause();
   })
 });
-// const createSnow = (container) => {
-//   const snowEl = document.createElement('span');
-//   snowEl.className = 'snow';
-//   const minSize = 5;
-//   const maxSize = 10;
-//   const size = Math.random() * (maxSize - minSize) + minSize;
-//   snowEl.style.width = `${size}px`;
-//   snowEl.style.height = `${size}px`;
-//   snowEl.style.left = Math.random() * 100 + '%';
-//   container.appendChild(snowEl);
-
-//   // 一定時間が経てば雪を消す
-//   setTimeout(() => {
-//     snowEl.remove();
-//   }, 10000);
-// }
